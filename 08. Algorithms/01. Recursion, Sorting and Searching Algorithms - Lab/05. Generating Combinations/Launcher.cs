3399c1960d2eb6b91ad854b92bf868ba05f8f7b3
WhitestWhitePersonWithWhiteFather <pers267@abv.bg>
2019-03-14 17:31:04 +0200

    05. Generating Combinations

--- 08. Algorithms/01. Recursion, Sorting and Searching Algorithms - Lab/05. Generating Combinations/Launcher.cs
+++ 08. Algorithms/01. Recursion, Sorting and Searching Algorithms - Lab/05. Generating Combinations/Launcher.cs
@@ -7,18 +7,29 @@ namespace _05._Generating
     {
         static void Main(string[] args)
         {
-            int[] set = new int[] { };
-            int[] vector = new int[] { };
+            int[] set = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int border = int.Parse(Console.ReadLine());
+            int[] vector = new int[border];
 
             int index = 0;
-            int border = 0;
 
-            GenCombs(set, vector, index, border);
+            GenCombs(set, vector, index, index);
         }
 
         private static void GenCombs(int[] set, int[] vector, int index, int border)
         {
-
+            if(index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = border; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+                    GenCombs(set, vector, index + 1, i + 1);
+                }
+            }
         }
     }
 }
