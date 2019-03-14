25e9608eb48f0086613caef75065f54a435dd462
WhitestWhitePersonWithWhiteFather <pers267@abv.bg>
2019-03-14 21:55:14 +0200

    Combinations with Repetition

---
+++ 08. Algorithms/02. Recursion, Sorting and Searching Algorithms - Exercise/03. Combinatio/Launcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Combinatio
+{
+    class Launcher
+    {
+        private static int[] combinations;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            combinations = new int[k];
+
+            GetCombinations(n);
+        }
+
+        private static void GetCombinations(int n, int index = 0, int element = 1)
+        {
+            if(index == combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = element; i <= n; i++)
+            {
+                combinations[index] = i;
+
+                GetCombinations(n, index + 1, i);
+            }
+        }
+    }
+}
