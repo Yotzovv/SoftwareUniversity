13b36e7f84fe62bb5c155deb4e72341a6fa0b175
WhitestWhitePersonWithWhiteFather <pers267@abv.bg>
2019-03-16 14:44:30 +0200

    4, 5th exercises

---
+++ 08. Algorithms/02. Recursion, Sorting and Searching Algorithms - Exercise/04. Towers of Hanoi/Launcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Towers_of_Hanoi
+{
+    class Launcher
+    {
+        private static Stack<int> source = new Stack<int>();
+        private static Stack<int> destination = new Stack<int>();
+        private static Stack<int> spare = new Stack<int>();
+
+        private static int steps = 0;
+
+        static void Main(string[] args)
+        {
+            int bottomDisk = int.Parse(Console.ReadLine());
+            source = new Stack<int>(Enumerable.Range(1, bottomDisk).Reverse());
+
+            PrintRods();
+            MoveDisks(bottomDisk, source, destination, spare);
+        }
+
+        private static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> destination, Stack<int> spare)
+        {
+            if(bottomDisk == 1)
+            {
+                steps++;
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{steps}: Moved disk");
+                PrintRods();
+            }
+            else
+            {
+                MoveDisks(bottomDisk - 1, source, spare, destination);
+                steps++;
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{steps}: Moved disk");
+                PrintRods();
+                MoveDisks(bottomDisk - 1, spare, destination, source);
+            }
+        }
+
+        private static void PrintRods()
+        {
+            Console.WriteLine("Source: {0}", string.Join(", ", source.Reverse()));
+            Console.WriteLine("Destination: {0}", string.Join(", ", destination.Reverse()));
+            Console.WriteLine("Spare: {0}", string.Join(", ", spare.Reverse()));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 08. Algorithms/02. Recursion, Sorting and Searching Algorithms - Exercise/05. Combinations/Launcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Combinations
+{
+    class Launcher
+    {
+        private static int[] combinations;
+        private static int n;
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new int[k];
+
+            GetCombinations();
+        }
+
+        private static void GetCombinations(int index = 0, int element = 1)
+        {
+            if (index == k)
+            {
+                if (!combinations.GroupBy(n => n).Any(group => group.Count() > 1))
+                {
+                    Console.WriteLine(string.Join(" ", combinations));
+                }
+
+                return;
+            }
+
+            for (int i = element; i <= n; i++)
+            {
+                combinations[index] = i;
+
+                GetCombinations(index + 1, i);
+            }
+        }
+    }
+}
